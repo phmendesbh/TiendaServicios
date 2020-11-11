1c73d03e1fbb9820ddd58bd9d56ddfc5896a9ce0
Paulo Mendes <motoredi@yahoo.com.br>
2020-11-11 12:43:45 -0300

    Criação do Serviço Utilizando SQL Server

---
+++ TiendaServicios.Api.Libro/Aplicacion/LibroMaterialDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TiendaServicios.Api.Libro.Aplicacion
+{
+    public class LibroMaterialDto
+    {
+        public Guid? LibreriaMaterialId { get; set; }
+
+        public string Titulo { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+
+        public Guid? AutorLibro { get; set; }
+    }
+}

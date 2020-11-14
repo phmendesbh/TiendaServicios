18a4ef0c15ea41797ff122d174de1837828f2cfe
Paulo Mendes <motoredi@yahoo.com.br>
2020-11-13 21:49:35 -0300

    Este cambio para azure devops ci

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -16,6 +16,7 @@ namespace TiendaServicios.Api.Libro.Tests
     {
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
+            // este metodo es para llenar con data de genful
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
                 .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });

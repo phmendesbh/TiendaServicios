41a509ef94fa9b30ddbee8c172f4f91bce596768
Paulo Mendes <motoredi@yahoo.com.br>
2021-05-18 21:06:28 -0300

    ApiGateway-MessageHandler

---
+++ TiendaServicios.Api.Gateway/LibroRemote/AutorModeloRemote.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TiendaServicios.Api.Gateway.LibroRemote
+{
+    public class AutorModeloRemote
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string AutorLibroGuid { get; set; }
+    }
+}
